6faabef3059bb8a43f0a912cca99ab48386f40a8
Michael Juliano <juliano.michael@gmail.com>
2022-07-16 21:04:48 -0400

    Made it easier to specify Xmx and Xms in userdata.
    By using JDK_JAVA_OPTIONS with a default value, it is now easier
    to specify Xms and Xmx in userdata by appending to the config
    file.

--- files/minecraft.sh
+++ files/minecraft.sh
@@ -2,5 +2,6 @@
 
 source ./config/config
 source $HOME/.sdkman/bin/sdkman-init.sh
+JDK_JAVA_OPTIONS=${JDK_JAVA_OPTIONS:-'-Xms1024M -Xmx2048M'}
 
-java -Xmx2048M -Xms1024M -jar server.jar --nogui --universe $MINECRAFT_UNIVERSE --world $MINECRAFT_WORLD
+java -jar server.jar --nogui --universe $MINECRAFT_UNIVERSE --world $MINECRAFT_WORLD
