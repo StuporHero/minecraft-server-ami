76ca50151853c58a40e368158bcfb5f6d356e60e
Michael Juliano <juliano.michael@gmail.com>
2022-07-16 21:12:23 -0400

    Tweaked config.
    Hardcoded the universe since that won't change per instance.
    Moved the default setting of MINECRAFT_WORLD to minecraft.sh so it
    can be overridden by a value set in /etc/minecraft/config

--- files/minecraft.sh
+++ files/minecraft.sh
@@ -2,6 +2,8 @@
 
 source ./config/config
 source $HOME/.sdkman/bin/sdkman-init.sh
+
+MINECRAFT_WORLD=${MINECRAFT_WORLD:-default}
 JDK_JAVA_OPTIONS=${JDK_JAVA_OPTIONS:-'-Xms1024M -Xmx2048M'}
 
-java -jar server.jar --nogui --universe $MINECRAFT_UNIVERSE --world $MINECRAFT_WORLD
+java -jar server.jar --nogui --universe /var/minecraft/universe --world $MINECRAFT_WORLD
