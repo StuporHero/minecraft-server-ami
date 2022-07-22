StuporHero/minecraft-server-ami

commit 7e87a65fba0f9ecb1fdded9b30a5f9055578f457

Tweaked the JVM parameters to try to improve latency.
Switched to the ZGC garbage collector. Added UseLargePages with a
script to allocate large pages. Automated the heap amount
calculation and set Xms and Xmx to the same value. Added
AlwaysPreTouch.

01-root.sh
@@ -17,6 +17,11 @@ mkdir /var/minecraft /var/log/minecraft /opt/minecraft /etc/minecraft
 chown minecraft:minecraft /var/minecraft /var/log/minecraft /opt/minecraft /etc/minecraft
 chown minecraft:minecraft /tmp/eula.txt /tmp/config /tmp/server.properties /tmp/minecraft.sh
 
+# Prepare the hugepages script (runs before minecraft)
+chmod root:root /tmp/hugepages.sh
+chmod 774 /tmp/hugepages.sh
+mv /tmp/hugepages.sh /opt/minecraft/
+
 # Create the systemd service for minecraft
 chown root:root /tmp/minecraft.service
 mv /tmp/minecraft.service /etc/systemd/system/minecraft.service

02-minecraft.sh
@@ -11,7 +11,7 @@ cd /opt/minecraft
 wget -O server.jar $download_url
 mv  /tmp/eula.txt /tmp/minecraft.sh ./
 mv /tmp/config /tmp/server.properties /etc/minecraft/
-chmod +x ./minecraft.sh
+chmod 774 ./minecraft.sh
 ln -s /etc/minecraft config
 ln -s /var/log/minecraft logs
 ln -s /etc/minecraft/server.properties server.properties

files/config
@@ -1,1 +1,11 @@
-# Set values for the minecraft server
+# Set memory value; values is in MiB
+TOTAL_MEMORY_SIZE=$(free -m | grep Mem: | awk '{print $2}')
+
+# Leaves room for the OS and JVM bits other than the heap
+HEAP_SIZE=$((TOTAL_MEMORY_SIZE - 2048))M
+
+# A custom Minecraft world may be added to this file through user_data.
+# For example:
+
+# #!/usr/bin/env bash
+# echo 'MINECRAFT_WORLD=custom' >> /etc/minecraft/config

files/hugepages.sh
@@ -1,0 +1,18 @@
+#!/usr/bin/env bash
+
+source ./config/config
+
+HUGE_PAGE_SIZE=$(grep Hugepagesize /proc/meminfo | awk '{print $2}')
+HUGE_PAGE_UNIT=$(grep Hugepagesize /proc/meminfo | awk '{print $3}')
+
+if [[ ${HUGE_PAGE_UNIT,,} = 'kb' ]]
+then
+    HUGE_PAGE_SIZE_MB=$((HUGE_PAGE_SIZE / 1024))
+elif [[ ${HUGE_PAGE_UNIT,,} = 'mb' ]]
+then
+    HUGE_PAGE_SIZE_MB=$HUGE_PAGE_SIZE
+fi
+
+PAGES_TO_ALLOCATE=$(((TOTAL_MEMORY_SIZE - 1024) / $HUGE_PAGE_SIZE))
+
+echo $PAGES_TO_ALLOCATE > /proc/sys/vm/nr_hugepages

files/minecraft.service
@@ -4,9 +4,11 @@ After=cloud-final.service
 
 [Service]
 WorkingDirectory=/opt/minecraft
-ExecStart=/opt/minecraft/minecraft.sh
+ExecStartPre=/opt/minecraft/hugepages.sh
+PermissionsStartOnly=true
 User=minecraft
 Group=minecraft
+ExecStart=/opt/minecraft/minecraft.sh
 
 [Install]
 WantedBy=cloud-init.target

files/minecraft.sh
@@ -4,6 +4,5 @@ source ./config/config
 source $HOME/.sdkman/bin/sdkman-init.sh
 
 MINECRAFT_WORLD=${MINECRAFT_WORLD:-default}
-JDK_JAVA_OPTIONS=${JDK_JAVA_OPTIONS:-'-Xms1024M -Xmx2048M'}
 
-java -jar server.jar --nogui --universe /var/minecraft/universe --world $MINECRAFT_WORLD
+exec java -XX:+UseLargePages -XX:+UseZGC -XX:+AlwaysPreTouch -Xms$HEAP_SIZE -Xmx$HEAP_SIZE -jar server.jar --nogui --universe /var/minecraft/universe --world $MINECRAFT_WORLD

minecraft-server-ami.pkr.hcl
@@ -68,6 +68,11 @@ build {
   }
 
   provisioner "file" {
+    source = "files/hugepages.sh"
+    destination = "/tmp/hugepages.sh"
+  }
+
+  provisioner "file" {
     source = "files/minecraft.sh"
     destination = "/tmp/minecraft.sh"
   }

test/test_myinfra.py
@@ -17,3 +17,18 @@ def test_should_have_service_minecraft_that_is_enabled(host):
 
 def test_should_have_cloud_init_enabled(host):
     assert host.service('cloud-init').is_enabled
+
+
+def test_should_have_executable_file_minecraft_sh_that_belongs_to_uid_gid_1001(host):
+    file = host.file('/opt/minecraft/minecraft.sh')
+    assert file.is_file
+    assert file.uid == 1001
+    assert file.gid == 1001
+    assert file.mode == 0o774
+
+def test_should_have_executable_file_hugepages_sh_that_belongs_to_root(host):
+    file = host.file('/opt/minecraft/hugepages.sh')
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o774

commit 56aadac8e3f517eb8dd0870897979840a46f2137

Fixed a bug introduced in 7e87a65fba0f9ecb1fdded9b30a5f9055578f457
Accidentally used chmod instead of chown to change the owner of
hugepages.sh.

01-root.sh
@@ -18,7 +18,7 @@ chown minecraft:minecraft /var/minecraft /var/log/minecraft /opt/minecraft /etc/
 chown minecraft:minecraft /tmp/eula.txt /tmp/config /tmp/server.properties /tmp/minecraft.sh
 
 # Prepare the hugepages script (runs before minecraft)
-chmod root:root /tmp/hugepages.sh
+chown root:root /tmp/hugepages.sh
 chmod 774 /tmp/hugepages.sh
 mv /tmp/hugepages.sh /opt/minecraft/
 